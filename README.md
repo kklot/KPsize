2ce611edc5ef1fbd5a9a5b5a08c1d85d6641c3bc
Kinh Nguyen <fieeuvaan@gmail.com>
2020-07-02 07:34:38 +0100

    add R

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # KPsize
 Sex initiation curve for UNAIDS key population estimates
+
+- data/AFS_full.csv.bz2 is curated from three survey sources: DHS, NSFG and
+  HBSC surveys. Please request the respective agencies for data contact k.nguyen at imperial.ac.uk the for data preparation.
---
+++ run.R
@@ -0,0 +1,47 @@
+rm(list=ls())
+
+require(INLA)
+require(R.utils)
+require(data.table)
+
+# data
+# -------------------------------------------------------------------------
+mydat <- fread('data/AFS_full.csv.bz2')
+nbmat <- readRDS('data/nbmat.rds')
+
+mydat[, .N, ISO_A2] # included missing data countries to predict
+mydat[, .(svy_weight = sum(scaled_w), .N), ISO_A2]
+
+# INLA options
+#-----------------------------------------------------------------------------
+inla.setOption("enable.inla.argument.weights", TRUE)
+assign("enable.model.likelihood.loglogisticsurv", TRUE, envir=INLA:::inla.get.inlaEnv())
+c_inla <- list(strategy = "gaussian", int.strategy='eb')
+
+# Model and priors
+#-----------------------------------------------------------------------------
+c_fami <- list(variant=1, hyper=list(theta=list(param=c(18, 1), prior='loggamma')))
+
+fm <- inla.surv(afs, event) ~ 1 + 
+    f(subrgid, model='iid', hyper=list(prec = list(prior = "pc.prec"))) +
+    f(cid, model="besag", graph=nbmat, constr=TRUE, adjust.for.con.comp = FALSE, scale.model=TRUE)
+
+inla.setOption('smtp', 'tauc')
+
+fit_all_female = inla(fm,
+    family='loglogisticsurv',
+    data = mydat[sex==2],
+    weight = mydat[sex==2, scaled_w],
+    control.inla = c_inla,
+    control.family = c_fami,
+    control.compute = c_comp,
+    num.threads = 10, verbose=TRUE)
+
+fit_all_male = inla(fm,
+    family='loglogisticsurv',
+    data = mydat[sex==1],
+    weight = mydat[sex==2, scaled_w],
+    control.inla = c_inla,
+    control.family = c_fami,
+    control.compute = c_comp,
+    num.threads = 10, verbose=TRUE)
